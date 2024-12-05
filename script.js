70f62c2d14398b96da12301621893837be91fcff
PatrizioFiori <patrizio.fiori1995@gmail.com>
2024-12-05 12:40:43 +0100

    JS completo (def)

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 const endpoint = ("https://jsonplaceholder.typicode.com/photos?_limit=6")
 
+
+let containerCarteUtenti = ""
+
 axios.get(endpoint)
-    .then(function(response){
-        console.log(response.data)
+    .then(function(response) {
+        response.data.forEach(element => inserimentoFotoTitolo(element));
     })
+
+
+    .catch(function(error) {
+        console.log(error);
+    });
+
+
+    function inserimentoFotoTitolo(element){
+        const thumb = element.thumbnailUrl
+        const titolo = element.title
+        
+        /*  per domani
+        const albumId = element.albumId 
+        const id = element.id
+        const url = element.url
+        console.log(albumId, id, titolo, url, thumb);
+        */
+        
+        containerCarteUtenti +=
+                            `<div class="col-md-4">
+                                <div class="card-utente">
+                                    <img src=${thumb} alt="" class="img">
+                                    <p class="mt-2">${titolo}</p>
+                                </div>
+                            </div>`
+
+        document.querySelector(".sezioneRow").innerHTML = containerCarteUtenti        
+        
+    }
