919696dab989978171fc3a3d4c01fbf6c8a01578
PatrizioFiori <patrizio.fiori1995@gmail.com>
2024-12-06 16:44:44 +0100

    Lavoro sull'overlay

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const endpoint = ("https://jsonplaceholder.typicode.com/photos?_limit=6")
 
 let containerCarteUtenti = ""
 let overlayOnOff = true
-let overlayContainer = "" 
 
 axios.get(endpoint)
     .then(function(response) {
